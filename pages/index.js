5321d9a1bf330a5390914a922a73248cdbf069d7
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-04-12 13:38:00 +0200

    Replacing dummy posts with featured and all posts with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,24 @@
 import FeaturedPosts from "../components/home-page/featured-posts";
-
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../utils/posts-utils";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Title 1",
-    image: "getting-started-nextjs.png",
-    excerpt: "Title text 1",
-    data: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Title 2",
-    image: "getting-started-nextjs.png",
-    excerpt: "Title text 2",
-    data: "2023-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Title 3",
-    image: "getting-started-nextjs.png",
-    excerpt: "Title text 3",
-    data: "2023-02-10",
-  },
-];
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <div>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </div>
   );
+};
+
+export const getStaticProps = () => {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 };
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,31 +1,18 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../utils/posts-utils";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Title 1",
-    image: "getting-started-nextjs.png",
-    excerpt: "Title text 1",
-    data: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Title 2",
-    image: "getting-started-nextjs.png",
-    excerpt: "Title text 2",
-    data: "2023-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Title 3",
-    image: "getting-started-nextjs.png",
-    excerpt: "Title text 3",
-    data: "2023-02-10",
-  },
-];
+const AllPostsPage = (props) => {
+  return <AllPosts posts={props.posts} />;
+};
 
-const AllPostsPage = () => {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export const getStaticProps = () => {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 };
 
 export default AllPostsPage;
