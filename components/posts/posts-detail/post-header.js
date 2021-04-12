a2167613e8f90c3417af1bc3af271eb72c415be6
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-04-12 12:57:28 +0200

    Setting up Post Content

---
+++ components/posts/posts-detail/post-header.js
@@ -0,0 +1,19 @@
+import classes from "./styles/post-detail.module.css";
+import Image from "next/image";
+const PostHeader = (props) => {
+  const { title, image } = props;
+  return (
+    <header className={classes.header}>
+      <h1>{title}</h1>
+      <Image
+        src={image}
+        alt={title}
+        width={200}
+        height={150}
+        layout={"responsive"}
+      />
+    </header>
+  );
+};
+
+export default PostHeader;
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,7 @@
-const PostDetail = () => {};
+import PostContent from "../../components/posts/posts-detail/post-content";
+
+const PostDetail = () => {
+  return <PostContent />;
+};
 
 export default PostDetail;
