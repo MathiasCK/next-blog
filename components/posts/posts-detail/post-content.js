a93240e19c91e4dcb29d4bc4f704e05506ca398d
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-04-13 17:50:41 +0200

    Size fixes

--- components/posts/posts-detail/post-content.js
+++ components/posts/posts-detail/post-content.js
@@ -2,11 +2,19 @@ import classes from "./styles/post-detail.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import Head from "next/head";
 
 // Themes for mastering javascript blogpost
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+// Instead of
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism/";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 const PostContent = (props) => {
   const { post } = props;
