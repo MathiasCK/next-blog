db6ecd4d56807c4fa5c604bbab96954ca29b8b89
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-04-13 13:34:55 +0200

    Setting up mongodb

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,59 @@
+import { useRef } from "react";
 import classes from "./styles/contact.module.css";
 
 const ContactForm = () => {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const messageRef = useRef();
+
+  const registrationHandler = (e) => {
+    e.preventDefault();
+
+    const userEmail = emailRef.current.value;
+    const userName = nameRef.current.value;
+    const userMessage = messageRef.current.value;
+
+    fetch("api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: userEmail,
+        name: userName,
+        message: userMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    alert("Message sent");
+    emailRef.current.value = "";
+    nameRef.current.value = "";
+    messageRef.current.value = "";
+  };
   return (
     <section className={classes.contact}>
       <h1>How can i help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={registrationHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input ref={emailRef} type="email" id="email" required />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input ref={nameRef} type="text" id="name" required />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea type="text" id="message" required rows="5" />
+          <textarea
+            ref={messageRef}
+            type="text"
+            id="message"
+            required
+            rows="5"
+          />
         </div>
         <div className={classes.actions}>
           <button>Send message</button>
---
+++ pages/api/contact.js
@@ -0,0 +1,54 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid user credentials!" });
+      return;
+    }
+
+    // Store in database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://MathiasCK:Xtrmck123@cluster0.gztjm.mongodb.net/nextBlog?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage._id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed" });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({ message: "Success stored", data: newMessage });
+  }
+};
+
+export default handler;
