275087955de656a81c7d18fc61ec2cbf76338826
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-04-13 17:16:01 +0200

    Extra meta

--- components/posts/all-posts.js
+++ components/posts/all-posts.js
@@ -1,12 +1,18 @@
 import PostsGrid from "./posts-grid";
 import classes from "./styles/posts.module.css";
+import Head from "next/head";
 
 const AllPosts = (props) => {
   return (
-    <section className={classes.posts}>
-      <h1>All Posts</h1>
-      <PostsGrid posts={props.posts} />
-    </section>
+    <div>
+      <Head>
+        <meta name="description" content="All my posts!" />
+      </Head>
+      <section className={classes.posts}>
+        <h1>All Posts</h1>
+        <PostsGrid posts={props.posts} />
+      </section>
+    </div>
   );
 };
 
